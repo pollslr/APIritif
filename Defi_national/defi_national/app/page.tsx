67573b78da28d339993e04967e1e5d193f589397
TeunHarm <CracketSnif2015@gmail.com>
2025-12-05 00:26:26 +0100

    Add parallax package

--- Defi_national/defi_national/app/page.tsx
+++ Defi_national/defi_national/app/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { useState, useEffect } from 'react';
 import Image from "next/image";
 import Button from "./components/button";
 import {ReactNode} from "react";
@@ -9,47 +8,38 @@ import QCM from "@/app/components/qcm";
 import Link from "next/link";
 import '@/app/style.css';
 
+import { Parallax } from 'react-parallax';
+
 export default function Home() {
-    const [scrollY, setScrollY] = useState(0);
-
-    useEffect(() => {
-        const handleScroll = () => {
-            setScrollY(window.scrollY);
-        };
-
-        window.addEventListener('scroll', handleScroll);
-        return () => window.removeEventListener('scroll', handleScroll);
-    }, []);
-
-    const parallaxSpeed = scrollY * 0.5;
-
     return (
     <div className="flex min-h-screen items-center justify-center font-sans">
         <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
-            <div className="Logo">
-                <Link href="/"><Image src='/img/nird_logo.png' alt="Logo NIRD" width={484} height={200} className="justify-center"/></Link>
-            </div>
-            <div>
-                <h2>Qui sommes-nous ?</h2>
-            </div>
-            
-            <Section title={""} content={""}>
+            <Parallax bgImage="/path/to/another/image">
+                <div className="Logo">
+                    <Link href="/"><Image src='/img/nird_logo.png' alt="Logo NIRD" width={484} height={200} className="justify-center"/></Link>
+                </div>
+                <div>
+                    <h2>Qui sommes-nous ?</h2>
+                </div>
                 
-            </Section>
-            
-            <Button text={"Test"}/>
-
-            <Section title={""} content={""}>
-                <QCM question={"Qui est correcte?"} reponses={[{"Oui": true, "Faux": false, "Incorrecte": false}]}/>
-            </Section>
-            
-            <Section title={"Contribuez"} content={""}/>
+                <Section title={""} content={""}>
+                    
+                </Section>
+                
+                <Button text={"Test"}/>
+        
+                <Section title={""} content={""}>
+                    <QCM question={"Qui est correcte?"} reponses={[{"Oui": true, "Faux": false, "Incorrecte": false}]}/>
+                </Section>
+                
+                <Section title={"Contribuez"} content={""}/>
+            </Parallax>
         </main>
     </div>
     );
 }
 
-function Section({title, content, children}: {title: string, content: string, children?: Element[]}) {
+function Section({title, content, children}: {title: string, content: string, children?: ReactNode}) {
     return (
         <div>
             <h2>{title}</h2>
