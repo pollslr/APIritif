a926776fab99d1d19a2cce737ffe2c7022c7b225
TeunHarm <CracketSnif2015@gmail.com>
2025-12-04 23:49:23 +0100

    Update page.tsx

--- Defi_national/defi_national/app/page.tsx
+++ Defi_national/defi_national/app/page.tsx
@@ -24,23 +24,8 @@ export default function Home() {
     const parallaxSpeed = scrollY * 0.5;
 
     return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-        <div
-            className="fixed top-0 left-0 w-full h-screen overflow-hidden -z-10"
-            style={{transform: `translateY(${parallaxSpeed}px)`, willChange: 'transform'}}>
-            <img
-                src={'https://www.stade-lavallois.com/wp-content/uploads/2023/04/une.jpg'}
-                alt="Background"
-                style={{
-                    width: '100%',
-                    height: '100%',
-                    objectFit: 'cover',
-                    objectPosition: 'center'
-                }}
-            />
-        </div>
-        
-        <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <div className="flex min-h-screen items-center justify-center font-sans">
+        <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
             <div className="Logo">
                 <Link href="/"><Image src='/img/nird_logo.png' alt="Logo NIRD" width={484} height={200} className="justify-center"/></Link>
             </div>
